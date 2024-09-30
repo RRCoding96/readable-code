686d36ec48d97e96e48ec501b8f93eebcf9210ab
김윤욱 <reznov03048@gmail.com>
2024-09-30 23:23:23 +0900

    ISP: Interface Segregation Principle

---
+++ src/main/java/cleancode/minesweeper/tobe/game/AnotherGame.java
@@ -0,0 +1,16 @@
+package cleancode.minesweeper.tobe.game;
+
+import cleancode.minesweeper.tobe.game.GameRunnable;
+
+public class AnotherGame implements GameRunnable {
+
+//    @Override
+//    public void initialize() {
+//        // 필요없음 -> 분리해야 됨 : ISP 위반
+//    }
+
+    @Override
+    public void run() {
+
+    }
+}
