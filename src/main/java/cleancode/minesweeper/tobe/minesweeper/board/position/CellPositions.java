b9c40942477af20d3257d102112aeb5b18deaf40
김윤욱 <reznov03048@gmail.com>
2024-10-04 21:41:40 +0900

    패키지 나누기

---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/position/CellPositions.java
@@ -0,0 +1,58 @@
+package cleancode.minesweeper.tobe.minesweeper.board.position;
+
+import cleancode.minesweeper.tobe.minesweeper.board.cell.Cell;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CellPositions {
+
+    private final List<CellPosition> cellPositions;
+
+    private CellPositions(List<CellPosition> cellPositions) {
+        this.cellPositions = cellPositions;
+    }
+
+    public static CellPositions of(List<CellPosition> cellPositions) {
+        return new CellPositions(cellPositions);
+    }
+
+    public static CellPositions from(Cell[][] board) {
+        List<CellPosition> cellPositions = new ArrayList<>();
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                CellPosition cellPosition = CellPosition.of(row, col);
+                cellPositions.add(cellPosition);
+            }
+        }
+
+        return of(cellPositions);
+    }
+
+    public List<CellPosition> extractRandomPositions(int count) {
+        List<CellPosition> cellPositions = new ArrayList<>(this.cellPositions);
+        Collections.shuffle(cellPositions);
+        return cellPositions.subList(0, count);
+    }
+
+
+    public List<CellPosition> subtract(List<CellPosition> positionListToSubtract) {
+        List<CellPosition> cellPositions = new ArrayList<>(this.cellPositions);
+
+        CellPositions positionsToSubtract = CellPositions.of(positionListToSubtract);
+
+        return cellPositions.stream()
+            .filter(positionsToSubtract::doesNotContain)
+            .toList();
+    }
+
+    private boolean doesNotContain( CellPosition position) {
+        return !cellPositions.contains(position);
+    }
+
+    public List<CellPosition> getCellPositions() {
+        return new ArrayList<>(cellPositions);
+    }
+}
